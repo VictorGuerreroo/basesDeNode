eaa498ba8277086a785f96aee264caae2ae09b00
Victor <v.g.m_@hotmail.com>
2021-04-02 19:29:08 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+console.clear();
+console.log('====================');
+console.log('   Tabla del:5');
+console.log('====================');
+for(let i = 1; i <= 10; i++) {
+
+    let primero = 5;
+  
+    let consola =`${primero} X ${i} = ${5*i}`;
+
+    console.log(consola);
+
+} 
+
